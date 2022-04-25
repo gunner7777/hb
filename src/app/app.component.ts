362d81c16a533f4f93b7a2bcc2f9ce80caf9e85a
gunner7777 <gunner1990@yandex.ru>
2022-04-25 23:06:28 +0300

    add height with window.innerHeight

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'grats';
+  innerHeight: number = 0;
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  ngOnInit() {
+    this.innerHeight = window.innerHeight;
+    console.log(this.innerHeight);
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
 }
